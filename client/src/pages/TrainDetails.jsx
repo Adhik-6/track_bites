20b1e7cb3b918ab7073f5a5891e7fc04c107efb2
Adhik <adhik.m10a@gmail.com>
2025-06-20 00:14:21 +0530

    Update TrainDetails.jsx

--- client/src/pages/TrainDetails.jsx
+++ client/src/pages/TrainDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/TrainDetails.css";
+import "../styles/trainDetails.css";
 import useGlobalStore from "../stores/global.stores";
 import logo from "./../assets/logo.jpg"; 
 
